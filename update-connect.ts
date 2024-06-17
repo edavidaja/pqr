a188055e2993ca0db5aedb75eb250c54e603ab21
edavidaja <david@posit.co>
2024-06-17 15:36:33 -0400

    add image publishing script

---
+++ update-connect.ts
@@ -0,0 +1,45 @@
+async function updateConnect(
+  server = Deno.env.get("CONNECT_SERVER"),
+  key = Deno.env.get("CONNECT_API_KEY"),
+  pythonVersion = Deno.env.get("PYTHON_VERSION"),
+  quartoVersion = Deno.env.get("QUARTO_VERSION"),
+  rVersion = Deno.env.get("R_VERSION"),
+): Promise<string> {
+  const installations = {
+    title: `Python ${pythonVersion} | Quarto ${quartoVersion} | R ${rVersion}`,
+    cluster_name: "Kubernetes",
+    name: `ghcr.io/edavidaja/pqr:python${pythonVersion}-quarto${quartoVersion}-r${rVersion}`,
+    python: {
+      installations: [{
+        path: `/opt/python/${pythonVersion}/bin/python`,
+        version: pythonVersion,
+      }],
+    },
+    quarto: {
+      installations: [{
+        path: `/opt/quarto/bin/quarto`,
+        version: quartoVersion,
+      }],
+    },
+    r: {
+      installations: [{
+        path: `/opt/R/${rVersion}/bin/R`,
+        version: rVersion,
+      }],
+    },
+  };
+  const body = JSON.stringify({ installations });
+
+  const response = await fetch(`${server}/__api__/v1/environments`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Key ${key}`,
+    },
+    body,
+  });
+
+  return response.json();
+}
+
+updateConnect();
